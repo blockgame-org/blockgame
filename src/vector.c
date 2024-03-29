345e773ff3dc9ebc10c3fd0374f34b7bc1fbe0c6
Benjamin Austin Jr <spidertyler1122@gmail.com>
2024-03-29 12:07:33 -0400

    add additional stuff to vector

--- src/vector.c
+++ src/vector.c
@@ -13,6 +13,25 @@ bg_vector(struct bg_vector *out, size_t element)
 }
 
 void
+bg_vector_drop_end(struct bg_vector *vec, size_t count)
+{
+    vec->length -= (count < vec->length) ? count : vec->length;
+}
+
+/// @brief calls cleanup function for all dropped elements
+/// @param vec
+/// @param count
+/// @param cleanup
+void
+bg_vector_drop_end_cleanup(struct bg_vector *vec, size_t count, void (*cleanup)(void *))
+{
+    for (size_t i = vec->length - count; i < vec->length; i++)
+        cleanup((char *)vec->raw + (i * vec->element));
+
+    bg_vector_drop_end(vec, count);
+}
+
+void
 bg_vector_grow(struct bg_vector *vec, size_t count)
 {
     assert(vec);
@@ -30,6 +49,15 @@ bg_vector_grow(struct bg_vector *vec, size_t count)
         vec->raw = bg_calloc(vec->capacity, vec->element);
     else
         vec->raw = bg_realloc(vec->raw, (vec->capacity * 2  + 1)* vec->element);
+}
+
+void
+bg_vector_shrink(struct bg_vector *vec)
+{
+    if (!vec->length)
+        return;
+    vec->raw = bg_realloc(vec->raw, (vec->length) * vec->element);
+    vec->capacity = vec->length;
 }
 
 void
@@ -51,4 +79,13 @@ bg_vector_free(struct bg_vector *vec)
 {
     free(vec->raw);
     memset(vec, 0, sizeof(struct bg_vector));
+}
+
+void
+bg_vector_cleanup(struct bg_vector *vec, void (*cleanup)(void *))
+{
+    for (size_t i = 0; i < vec->length; i++)
+        cleanup((char *)vec->raw + (i * vec->element));
+
+    bg_vector_free(vec);
 }
