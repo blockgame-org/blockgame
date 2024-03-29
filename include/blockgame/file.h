dc7ca997807d0bfe3531d2f4ce1d7c808e51639f
m4xine <deandrade@posteo.com>
2024-03-29 18:35:52 -0400

    Add `bg_read_file`

---
+++ include/blockgame/file.h
@@ -0,0 +1,11 @@
+#ifndef BLOCKGAME_FILE_H
+#define BLOCKGAME_FILE_H
+
+/// @brief Reads and allocates the contents of a file into a string.
+/// @param out Output for the newly allocated string with the file's contents.
+/// @param filename Name of the file to be opened.
+/// @return 
+int
+bg_read_file(char **out, char const *filename);
+
+#endif // BLOCKGAME_FILE_H
