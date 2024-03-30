7fd7ae0799b4b2e383528dedd5b6f305b0155b7d
m4xine <deandrade@posteo.com>
2024-03-30 11:33:58 -0400

    Add float vectors and 4x4 matrix

---
+++ include/blockgame/mathtypes.h
@@ -0,0 +1,10 @@
+#ifndef BLOCKGAME_MATHTYPES_H
+#define BLOCKGAME_MATHTYPES_H
+
+typedef float bg_vec2f[2];
+typedef float bg_vec3f[3];
+typedef float bg_vec4f[4];
+
+typedef bg_vec4f bg_mat4[4];
+
+#endif // BLOCKGAME_MATHTYPES_H
