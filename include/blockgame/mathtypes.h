18d3836f41d17dc3cb363a6edf76a532b20e29b4
m4xine <deandrade@posteo.com>
2024-03-30 23:03:01 -0400

    Add `bg_vec4u` `bg_quat`

--- include/blockgame/mathtypes.h
+++ include/blockgame/mathtypes.h
@@ -1,10 +1,14 @@
 #ifndef BLOCKGAME_MATHTYPES_H
 #define BLOCKGAME_MATHTYPES_H
 
+typedef unsigned int bg_vec4u[4];
+
 typedef float bg_vec2f[2];
 typedef float bg_vec3f[3];
 typedef float bg_vec4f[4];
 
 typedef bg_vec4f bg_mat4[4];
+
+typedef float bg_quat[4];
 
 #endif // BLOCKGAME_MATHTYPES_H
