962883dcf82650df9ddfa99a7aef0550ac47d6cd
m4xine <deandrade@posteo.com>
2024-03-31 21:18:53 -0400

    Run engine

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
-int main(int argc, char const **argv) {
-  (void)argc;
-  (void)argv;
+#include <blockgame/engine.h>
 
-  return 0;
+int main(int argc, char const **argv) {
+    (void)argc;
+    (void)argv;
+
+    bg_initLogger(stderr);
+
+    if (0 != bg_initEngine())
+        return 1;
+    if (0 != bgEngine_run())
+        return 1;
+
+    return 0;
 }
