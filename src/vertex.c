190d0563b488da7bda5df72d83f443c4f7f00ed8
m4xine <deandrade@posteo.com>
2024-04-06 21:20:20 -0400

    Fix vertex `relativeoffset` parameter

--- src/vertex.c
+++ src/vertex.c
@@ -10,11 +10,11 @@ int bgVertex_configureVaoVbo(int vao, int vbo, int bindingIx) {
     glEnableVertexArrayAttrib(vao, BG_VERTEX_UV_LOCATION);
 
     glVertexArrayAttribFormat(vao, BG_VERTEX_POSITION_LOCATION, 3, GL_FLOAT,
-                              GL_FALSE, 0);
-    glVertexArrayAttribFormat(vao, BG_VERTEX_NORMAL_LOCATION, 3, GL_FLOAT,
                               GL_FALSE, offsetof(bgVertex, position));
+    glVertexArrayAttribFormat(vao, BG_VERTEX_NORMAL_LOCATION, 3, GL_FLOAT,
+                              GL_FALSE, offsetof(bgVertex, normal));
     glVertexArrayAttribFormat(vao, BG_VERTEX_UV_LOCATION, 2, GL_FLOAT, GL_FALSE,
-                              offsetof(bgVertex, normal));
+                              offsetof(bgVertex, uv));
 
     glVertexArrayAttribBinding(vao, BG_VERTEX_POSITION_LOCATION, bindingIx);
     glVertexArrayAttribBinding(vao, BG_VERTEX_NORMAL_LOCATION, bindingIx);
